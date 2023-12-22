184fe0181fe7c0f478dd4da850af1d48557608c4
Soumyadeep Mallick <81796438+Soumya-2003@users.noreply.github.com>
2023-12-22 22:05:17 +0530

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ❤️ by{" "}
+      <a
+        href="https://www.github.com/Soumya-2003"
+        style={{ cursor: "pointer" }}
+      >
+        Soumyadeep
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
